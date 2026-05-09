8d2368e6139451878fb28df3167cc30a8e2a878b
danva16 <danieladannava111116@gmail.com>
2026-05-08 19:30:34 -0600

    Se revisa la edad del usuario y se le da su cálculo

--- 1.CalculadoraIndiceDeMasaCorporal.py
+++ 1.CalculadoraIndiceDeMasaCorporal.py
@@ -13,4 +13,13 @@ while pers > 0:
     height = float(input("Ingrese su altura en metros: "))
     #Calculamos el índice de masa corporal usando nuestra fórmula y guardamos en la variable IMC  
     IMC = weight / (height ** 2)
-    
+    #verficamos si el usuario es menor o mayor de edad
+    if(age < 18):
+        #Si el usuario es menor de edad, mostramos un mensaje indicando que no se puede calcular el IMC
+        print("Lo siento, no se puede calcular el IMC para menores de edad.")
+    elif(age >= 18):
+        #Mostramos el resultado del IMC
+        print(f"{name}, tu índice de masa corporal es: {IMC:.2f}")
+        #Mostramos un mensaje si los datos ingresados son erroneos
+    else:
+        print("Favor de verificar los datos ingresados.")
