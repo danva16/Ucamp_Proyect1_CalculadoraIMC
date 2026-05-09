d46983de98ac7645aebec879b3ed4c99c3706028
danva16 <danieladannava111116@gmail.com>
2026-05-08 19:50:10 -0600

    Ultimos ajustes en la sintaxis del proyecto

--- 1.CalculadoraIndiceDeMasaCorporal.py
+++ 1.CalculadoraIndiceDeMasaCorporal.py
@@ -24,24 +24,24 @@ while pers > 0:
     else:
         print("Favor de verificar los datos ingresados.")
 
-        print("Ahora con los datos obtenidos, se le hará su valoración")
-        #Verificamos el valor del IMC para mostrar un mensaje dependiendo del resultado
-        if 0 <= IMC <= 15.99:
-            print("Delgadez severa.")
-        elif 16 <= IMC < 16.99:
-            print("Delgadez moderada.")
-        elif 17 <= IMC < 18.49:
-            print("Delgadez leve.")
-        elif 18.5 <= IMC < 24.99:
-            print("Tienes un peso normal.")
-        elif 25 <= IMC < 29.99:
-            print("Tienes sobrepeso.")
-        elif 30 <= IMC < 34.99:
-            print("Tienes obesidad leve.")
-        elif 35 <= IMC < 39.99:
-            print("Tienes obesidad media.")
-        elif IMC >= 40:
-            print("Tienes obesidad mórbida.")
+    print("\nAhora con los datos obtenidos, se le hará su valoración\n")
+    #Verificamos el valor del IMC para mostrar un mensaje dependiendo del resultado
+    if 0 <= IMC <= 15.99:
+        print(f"{name}, tienes delgadez severa.")
+    elif 16 <= IMC < 16.99:
+        print(f"{name}, tienes delgadez moderada.")
+    elif 17 <= IMC < 18.49:
+        print(f"{name}, tienes delgadez leve.")
+    elif 18.5 <= IMC < 24.99:
+        print(f"{name}, tienes un peso normal.")
+    elif 25 <= IMC < 29.99:
+        print(f"{name}, tienes sobrepeso.")
+    elif 30 <= IMC < 34.99:
+        print(f"{name}, tienes obesidad leve.")
+    elif 35 <= IMC < 39.99:
+        print(f"{name}, tienes obesidad media.")
+    elif IMC >= 40:
+        print(f"{name}, tienes obesidad mórbida.")
 
     #Restamos 1 a la variable pers para que el ciclo se ejecute hasta que se hayan ingresado todas las personas
     pers -= 1
